1180b32d6c859a8cf6ff3e31c100c85304a71377
RileyDruken <110466630+RileyDruken@users.noreply.github.com>
2025-04-12 17:39:21 -0230

    Implemented Templates Added routes

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
+
+@app.route('/removal')
+def removal():
+    return render_template('removal.html')
 
 
 if __name__ == '__main__':
