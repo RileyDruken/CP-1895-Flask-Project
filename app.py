61f78da8c4d0ba0722e7f4855c09991c3037716b
RileyDruken <110466630+RileyDruken@users.noreply.github.com>
2025-04-13 23:26:15 -0230

    Added Games cards Added User session

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, session
 from werkzeug.utils import secure_filename
 import os,datetime
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = os.path.join(app.root_path, "static/uploads")
-app.config['MAX_CONTENT_LENGTH'] = 50000
+app.config['MAX_CONTENT_LENGTH'] = 160000
+app.secret_key = os.getenv("SECRET_KEY")
+
+gamesList = []
+
 
 @app.route('/')
 def index():
@@ -12,6 +16,9 @@ def index():
 
 @app.route('/games')
 def games():
+    if "games" in session:
+        print(session["games"])
+        return render_template('games.html', games=session["games"])
     return render_template('games.html')
 
 @app.route('/removal')
@@ -43,9 +50,12 @@ def add():
             game.append(genre)
             game.append(platform)
             game.append(uniquename)
-            print(game)
+
         else:
             return render_template('add.html', error="Image File type not supported or is empty")
+        gamesList = session['games']
+        gamesList.append(game)
+        session['games'] = gamesList
 
     return render_template('add.html')
 
